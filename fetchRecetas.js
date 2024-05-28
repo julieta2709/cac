520b3f0888819448869a47ceba46ef03e4659ab8
Julieta Gordillo <julietagordillo13@hotmail.com>
2024-05-28 15:09:12 -0300

    finish api connection

--- fetchRecetas.js
+++ fetchRecetas.js
@@ -2,6 +2,8 @@ const apiServer = "https://api.edamam.com";
 const apiId = "cff78ee0";
 const apiKey = "57ef1a787699d3e740fd90891c0f3135";
 
+let busquedaRealizada = false;
+
 function handleKeyDown(event) {
   if (event.key === "Enter") {
     buscarRecetas();
@@ -9,40 +11,46 @@ function handleKeyDown(event) {
 }
 
 async function buscarRecetas() {
+  busquedaRealizada = true;
   const query = document.getElementById("input").value;
   const url = `${apiServer}/search?q=${query}&app_id=${apiId}&app_key=${apiKey}`;
-  console.log("Realizando solicitud a la URL:", url);
 
   try {
     const response = await fetch(url);
-    console.log("Respuesta recibida:", response);
     const data = await response.json();
-    console.log(data);
     mostrarResultados(data.hits);
-    document
-      .getElementById("resultados")
-      .scrollIntoView({ behavior: "smooth" });
+
+    if (busquedaRealizada) {
+      document
+        .getElementById("resultados")
+        .scrollIntoView({ behavior: "smooth" });
+    }
   } catch (error) {
     console.error("Error al conectar con la API de Edamam:", error);
   }
 }
-buscarRecetas();
 
 function mostrarResultados(recetas) {
-  const resultadosDiv = document.getElementById("resultados");
+  const resultadosDiv = document.getElementById("resultadosCard");
   resultadosDiv.innerHTML = "";
 
-  if (recetas.length === 0) {
+  if (recetas.length === 0 && busquedaRealizada) {
     resultadosDiv.innerHTML = "<p>No se encontraron recetas.</p>";
     return;
   }
 
   recetas.forEach((receta) => {
     const recetaDiv = document.createElement("div");
+    recetaDiv.classList.add("col-lg-4", "menu-item");
     recetaDiv.innerHTML = `
-            <h2>${receta.recipe.label}</h2>
-            <img src="${receta.recipe.image}" alt="${receta.recipe.label}" style="width: 100px; height: 100px;">
-            <p><a href="${receta.recipe.url}" target="_blank">Ver receta completa</a></p>
+    <div class="card">
+    <img src="${receta.recipe.image}" class="card-img-top" alt="${receta.recipe.label}">
+    <div class="card-body">
+        <h5 class="card-title">${receta.recipe.label}</h5>
+        <p class="card-text">Descripción de la receta aquí.</p>
+        <a href="${receta.recipe.url}" class="btn" target="_blank">Receta</a>
+    </div>
+    </div>
         `;
     resultadosDiv.appendChild(recetaDiv);
   });
